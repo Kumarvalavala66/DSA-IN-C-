1a296dec70dc4bcba6dd7d4d388e17830d3efc2f
Kumarvalavala66 <prasannakumarvalavala@gmail.com>
2025-11-01 23:18:20 +0530

    Fix weight capacity check and update item prompts

--- knapsack.c
+++ knapsack.c
@@ -18,7 +18,7 @@ void knapsack_back(int index,int cur_weight,int cur_profit){
         return;
     }
     knapsack_back( index +1, cur_weight, cur_profit);
-    if(cur_weight + items[index].weight < capacity){
+    if(cur_weight + items[index].weight <= capacity){
     knapsack_back( index+1, cur_weight+items[index].weight, cur_profit+items[index].profit);
 
     }
@@ -30,9 +30,9 @@ int main(){
     printf("Enter the capacity   :");
     scanf("%d",&capacity);
     for(int i=0 ; i < n ; i++){
-        printf("Enter the weight of item at %d :",i);
+        printf("Enter the weight of item at %d :",i+1);
         scanf("%d",&items[i].weight);
-        printf("Enter the profit of item at %d :",i);
+        printf("Enter the profit of item at %d :",i+1);
         scanf("%d",&items[i].profit);
     }
     knapsack_back(0,0,0);
